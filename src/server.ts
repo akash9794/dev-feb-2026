822233853fe657f0ccf71474c50ca0e78144c40c
tHBP0007 <theHalfBloodPrince0007@gmail.com>
2026-02-14 12:46:20 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 function welcome(name: string) {
     console.log('Hello, ' + name)
+
+    const user = {
+        firstName: "Akash",
+    }
+
+    const fname = user.firstName;
 }
 
 welcome('akash')
